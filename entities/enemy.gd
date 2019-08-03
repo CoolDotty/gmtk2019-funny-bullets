a3b31ffbe0af0e7093554c3d64e8b80e9f49013e
Andrew Busto <andrewlbusto@gmail.com>
2019-08-03 10:44:37 -0700

    Add basic walk to enemy behavior

--- entities/enemy.gd
+++ entities/enemy.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
 export (PackedScene) var AmmoType
+export var speed = 200
 
 onready var Player = $"../player"
 onready var clipazine = get_parent()
 onready var Gun = $Gun
 
 var gun_has_my_ammo = false
+var walk_target = null
 
 func _ready():
 	Gun.clipazine = clipazine
@@ -18,9 +20,29 @@ func _physics_process(delta):
 	var intersected = space_state.intersect_ray(global_position, player_pos, ignored)
 	
 	if intersected.empty(): # We can see the player
-		var dir = Player.global_position - global_position
-		rotation = dir.angle()
-		shoot()
+		player_spotted_at(Player.global_position)
+	
+	if walk_target && not at_position(walk_target):
+		move_to(walk_target)
+
+func at_position(target_pos):
+	var x_distance = target_pos.x - position.x
+	var y_distance = target_pos.y - position.y
+	var distance = sqrt(pow(x_distance, 2) + pow(y_distance, 2))
+	
+	return distance < 100
+
+func move_to(target_pos):
+	var dir = target_pos - global_position
+	rotation = dir.angle()
+	var velocity = Vector2(speed, 0).rotated(rotation)
+	move_and_slide(velocity)
+
+func player_spotted_at(player_pos):
+	var dir = player_pos - global_position
+	walk_target = Vector2(player_pos.x, player_pos.y)
+	rotation = dir.angle()
+	shoot()
 
 func shoot():
 	if gun_has_my_ammo:
