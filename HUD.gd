7cf4b71eb21067e25439520403774f983fb9eb9b
KarlTheCool <karldamanphillips@gmail.com>
2019-08-03 22:32:34 -0700

    updated tutorial

--- HUD.gd
+++ HUD.gd
@@ -2,17 +2,18 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Global.death_count % 3 == 0:
+		$IntroLabel.show()
+	elif Global.death_count % 3 == 1:
+		$ReloadLabel.show()
+	elif Global.death_count % 3 == 2:
+		$RackLabel.show()
 
 func lose():
-	if Global.death_count == 1:
-		$Load_Fake_Label.visible = true
-	elif Global.death_count == 2:
-		$Shoot_Label.visible = true
-	elif Global.death_count == 3:
-		$Rack_Label.visible = true
-	else:
-		$Restart_Label.visible = true
+	$IntroLabel.hide()
+	$ReloadLabel.hide()
+	$RackLabel.hide()
+	$Restart_Label.show()
 
 func win():
-	$Win_Label.visible = true
+	$WinLabel.show()
